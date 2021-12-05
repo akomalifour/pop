374f273973fd7e53bd8f0e29cdc2a21098a07c53
akomalifour <95312948+akomalifour@users.noreply.github.com>
2021-12-05 12:36:28 +0700

    Add files via upload

---
+++ media/help.js
@@ -0,0 +1,128 @@
+const help = (prefix) => {
+	return`╭─❒ INFO DEVELOPER
+│
+│◦➛ Nama : Haris Setiawan
+│
+│◦➛ No : http://wa.me/6283144394823
+│
+│◦➛ Yt : https://bit.ly/3F6LoHb
+│
+│◦➛ Ig : https://bit.ly/3bYT9Cq
+│
+│◦➛ GRUP BOT : https://bit.ly/3qmZgbY
+│
+│◦➛ GRUP INFO : https://bit.ly/30I67Tc
+│
+└────────────────────❒
+
+
+╭─❒ ⋮☰ MENU
+│▬▭▬▭▬▭▬▭▬▬▭▬▭▬
+│   ❏SIMPLE MENU❏
+│◦➛⿻ ${prefix}menu/help
+│◦➛⿻ ${prefix}command
+│▬▭▬▭▬▭▬▭▬▬▭▬▭▬
+│   ❏GROUP MENU❏
+│◦➛⿻ ${prefix}antilink
+│◦➛⿻ ${prefix}welcome
+│◦➛⿻ ${prefix}group
+│◦➛⿻ ${prefix}linkgrup
+│◦➛⿻ ${prefix}promote
+│◦➛⿻ ${prefix}demote
+│◦➛⿻ ${prefix}add
+│◦➛⿻ ${prefix}kick
+│◦➛⿻ ${prefix}setpp
+│▬▭▬▭▬▭▬▭▬▬▭▬▭▬
+│   ❏STICKER MENU❏
+│◦➛⿻ ${prefix}attp
+│◦➛⿻ ${prefix}sticker
+│◦➛⿻ ${prefix}tomp3
+│◦➛⿻ ${prefix}tovideo
+│▬▭▬▭▬▭▬▭▬▬▭▬▭▬
+│   ❏RANDOM MENU❏
+│◦➛⿻ ${prefix}ttaudio
+│◦➛⿻ ${prefix}detikvn
+│◦➛⿻ ${prefix}toimg
+│◦➛⿻ ${prefix}tovideo
+│◦➛⿻ ${prefix}tomp3
+│◦➛⿻ ${prefix}detikvideo
+│◦➛⿻ ${prefix}kodenuklir
+│◦➛⿻ ${prefix}linkbokep
+│▬▭▬▭▬▭▬▭▬▬▭▬▭▬
+│   ❏OWNER❏
+│◦➛⿻ ${prefix}owner
+│◦➛⿻ ${prefix}sewabot
+│◦➛⿻ ${prefix}setpp
+│◦➛⿻ ${prefix}setthumb
+│◦➛⿻ ${prefix}upswtexs
+│◦➛⿻ ${prefix}upswvn
+│◦➛⿻ ${prefix}upswgif
+│◦➛⿻ ${prefix}upswaudio
+│◦➛⿻ ${prefix}upswlokasi
+│◦➛⿻ ${prefix}upswsticker
+│◦➛⿻ ${prefix}upswimage
+│◦➛⿻ ${prefix}upswvideo
+│◦➛⿻ ${prefix}bc2
+│◦➛⿻ ${prefix}tobc
+│◦➛⿻ ${prefix}set
+│◦➛⿻ ${prefix}bc
+│▬▭▬▭▬▭▬▭▬▬▭▬▭▬
+│   ❏DOWNLOAD❏
+│◦➛⿻ ${prefix}play
+│◦➛⿻ ${prefix}ytsearch
+│◦➛⿻ ${prefix}ytmp4
+│◦➛⿻ ${prefix}tiktok
+│◦➛⿻ ${prefix}pinterest
+│▬▭▬▭▬▭▬▭▬▬▭▬▭▬
+│   ❏ANIME❏
+│◦➛⿻ ${prefix}manga
+│◦➛⿻ ${prefix}character
+│◦➛⿻ ${prefix}anime
+│◦➛⿻ ${prefix}kusonimesearch
+│◦➛⿻ ${prefix}otakudesusearch
+│◦➛⿻ ${prefix}hentaisearch
+│◦➛⿻ ${prefix}nekopoisearch
+│▬▭▬▭▬▭▬▭▬▬▭▬▭▬
+│   ❏SEARCH❏
+│◦➛⿻ ${prefix}gimage
+│◦➛⿻ ${prefix}wallpapersearch
+│◦➛⿻ ${prefix}playstore
+│◦➛⿻ ${prefix}shopee
+│◦➛⿻ ${prefix}google
+│▬▭▬▭▬▭▬▭▬▬▭▬▭▬
+│   ❏PRIMBON❏
+│◦➛⿻ ${prefix}artinama
+│◦➛⿻ ${prefix}jodoh
+│◦➛⿻ ${prefix}jadian
+│▬▭▬▭▬▭▬▭▬▬▭▬▭▬
+│   ❏STALK MENU❏
+│◦➛⿻ ${prefix}stalkig
+│◦➛⿻ ${prefix}stalkgithub
+│◦➛⿻ ${prefix}stalktiktok
+│▬▭▬▭▬▭▬▭▬▬▭▬▭▬
+│   ❏RAMDOM MEME❏
+│◦➛⿻ ${prefix}randommeme
+│◦➛⿻ ${prefix}randomdarkjoke
+│◦➛⿻ ${prefix}randommemeindo
+│
+│▬▭▬▭▬▭▬▭▬▬▭▬▭▬
+│
+│▬▭▬▭▬▭▬▭▬▬▭▬▭▬
+│Sc di Yt Gw 🚶│
+│By : ʜᴀɴᴢ々ᴏꜰᴄ │
+│▬▭▬▭▬▭▬▭▬▬▭▬▭▬
+└────────────────────❒
+TQ TO Wong々Hore Team
+
+BY HANZ々OFC彡️`
+}
+exports.help = help
+
+
+
+
+
+
+
+
+
